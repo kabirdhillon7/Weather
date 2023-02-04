88e55a450e9184bd104dd13af1256f923187e8fa
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-02-03 23:36:07 -0800

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Kabir Dhillon on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
