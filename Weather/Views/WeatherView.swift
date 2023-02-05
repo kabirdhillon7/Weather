f9ca0b01d5e6c09991d8b66307e6d5dcc20d7218
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-02-04 20:20:51 -0800

    WeatherView
    Completed by sample NY data

--- Weather/Views/WeatherView.swift
+++ Weather/Views/WeatherView.swift
@@ -57,6 +57,34 @@ struct WeatherView: View {
             }
             .padding()
             .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack {
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Weather now")
+                        .bold().padding(.bottom)
+                    
+                    HStack {
+                        WeatherRow(logo: "thermometer.low", name: "Min Temp", value: (weather.main.tempMin.roundDouble() + "º"))
+                        Spacer()
+                        WeatherRow(logo: "thermometer.high", name: "Max Temp", value: (weather.main.tempMax.roundDouble() + "º"))
+                    }
+                    
+                    HStack {
+                        WeatherRow(logo: "wind", name: "Wind Speed", value: (weather.wind.speed.roundDouble() + "m/s"))
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value: (weather.main.humidity.roundDouble() + "%"))
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(red: 0.0, green: 0.094, blue: 0.475))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+                
+            }
         }
         .edgesIgnoringSafeArea(.bottom)
         .background(Color(red: 0.0, green: 0.094, blue: 0.475))
